c3cae7f29cd8e64b59148a66b4fd77ca8fd546e5
thanhnt <thanhnt@123xe.vn>
2019-03-11 17:49:37 +0700

    Fix typos

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func InitWithBSHostPort(bsHost, bsPort string) {
 	} else {
 		enable_getsig = false
 	}
-	controller.SetPrivateModel(models.NewtrustkeysprivatekvAcceptAllModel(bsHost, bsPort), enable_getsig)
+	controllers.SetPrivateModel(models.NewtrustkeysprivatekvAcceptAllModel(bsHost, bsPort), enable_getsig)
 }
 
 func main() {
